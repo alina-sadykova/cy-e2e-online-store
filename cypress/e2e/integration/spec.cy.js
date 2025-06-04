f9d109e03cfce920e7ba9ce669846ba38bfcb2aa
alina-sadykova <asadykovaa@gmail.com>
2025-06-04 16:49:30 -0500

    add test for new account form

--- cypress/e2e/integration/spec.cy.js
+++ cypress/e2e/integration/spec.cy.js
@@ -3,6 +3,20 @@ import UserPage from "../../pages/UserPage";
 describe("User registration page", () => {
   const userPage = new UserPage();
   const userLocators = userPage.locators;
+  let formLabels;
+  let userInfo;
+
+  before(() => {
+    cy.fixture("signUpFormLabels").then((data) => {
+      formLabels = data;
+      console.log("formLabels", formLabels);
+    });
+    cy.fixture("userAccountInfo").then((data) => {
+      userInfo = data;
+      console.log("userInfo", userInfo);
+    });
+  });
+
   /* 
   2. Navigate to url 'http://automationexercise.com'
   3. Verify that home page is visible successfully
@@ -10,25 +24,72 @@ describe("User registration page", () => {
   5. Verify 'New User Signup!' is visible
   6. Enter name and email address
   7. Click 'Signup' button
-  8. Verify that 'ENTER ACCOUNT INFORMATION' is visible */
+  */
 
-  it("TC01 - allows user to sign up using name and email", () => {
+  it("TC01 - allows user to sign up using name and email", function () {
     cy.title().should("eq", "Automation Exercise");
     userPage.clickSignUpOrLoginLink();
-    userLocators.getNewUserSignUpForm().should("be.visible");
-    userPage.fillOutSignUpForm("John", "johnp@gmail.com");
-    cy.url().should("contain", "/signup");
     userLocators
-      .getNewUserSignUpFormHeader()
-      .should("be.visible")
-      .and("have.text", "Enter Account Information");
+      .getNewUserSignUpHeader()
+      .should(
+        "have.text",
+        formLabels.find((el) => el.key === "signupHeader")?.value
+      );
+
+    userPage.fillSignUpForm(userInfo.fullname, userInfo.email);
+
+    cy.url().should("contain", "/signup");
+
+    userLocators.getAccountFormHeader().each((el) => {
+      cy.wrap(el).should("be.visible");
+    });
   });
 
   it("TC02 - does NOT allow invalid or already existing email", () => {
     cy.title().should("eq", "Automation Exercise");
     userPage.clickSignUpOrLoginLink();
     userLocators.getNewUserSignUpForm().should("be.visible");
-    userPage.fillOutSignUpForm("John", "john@gmail.com");
+    userPage.fillSignUpForm(userInfo.fullname, userInfo.existingEmail);
     userLocators.getEmailExistErrorMessage().should("be.visible");
+  });
+
+  /*
+  8. Verify that 'ENTER ACCOUNT INFORMATION' is visible 
+  9. Fill details: Title, Name, Email, Password, Date of birth
+  10. Select checkbox 'Sign up for our newsletter!'
+  11. Select checkbox 'Receive special offers from our partners!'
+  12. Fill details: First name, Last name, Company, Address, Address2, Country, State, City, Zipcode, Mobile Number
+  13. Click 'Create Account button'
+  14. Verify that 'ACCOUNT CREATED!' is visible
+  15. Click 'Continue' button
+  16. Verify that 'Logged in as username' is visible
+  17. Click 'Delete Account' button
+  18. Verify that 'ACCOUNT DELETED!' is visible and click 'Continue' button */
+
+  it("TC03 - allows to create a new account and delete it", () => {
+    userPage.clickSignUpOrLoginLink();
+    userPage.fillSignUpForm(userInfo.fullname, userInfo.email);
+
+    userLocators.getAccountFormHeader().each((el) => {
+      cy.wrap(el).should("be.visible");
+    });
+
+    userPage.enterAccountInfo(userInfo);
+
+    userPage.fillAddressForm(userInfo);
+
+    userLocators.getCreateAccountButton().click();
+    userLocators
+      .getAccountFormHeader()
+      .should(
+        "have.text",
+        formLabels.find((el) => el.key === "accountCreatedHeader")?.value
+      );
+
+    userLocators.getContinueButton().click();
+    userPage.checkLoggedinAs(userInfo.fullname);
+
+    userPage.deleteAccountandVerify();
+    userLocators.getContinueButton().click();
   });
 });
--- cypress/pages/UserPage.js
+++ cypress/pages/UserPage.js
@@ -1,6 +1,7 @@
 export default class UserPage {
   locators = {
     getSignUpOrLoginLink: () => cy.get("a[href='/login']"),
+    getNewUserSignUpHeader: () => cy.get(".signup-form h2"),
     getNewUserSignUpForm: () => cy.get(".signup-form"),
     getSignUpName: () => cy.getByDataQa("signup-name"),
     getSignUpName: () => cy.getByDataQa("signup-name"),
@@ -8,16 +9,115 @@ export default class UserPage {
     getSignUpButton: () => cy.getByDataQa("signup-button"),
     getEmailExistErrorMessage: () =>
       cy.contains("Email Address already exist!"),
-    getNewUserSignUpFormHeader: () => cy.get(".login-form>h2 b"),
+    getAccountFormHeader: () => cy.get("h2[class^='title']>b"),
+    getNewUserSignUpFormLabels: () => cy.get(".login-form label"),
+    getFormLabels: () => cy.ger("form label"),
+
+    // locators for account info form:
+    getRadioOptionGender: () => cy.get(".radio input"),
+    getTitleMr: () => cy.get('[value="Mr"]'),
+    getTitleMrs: () => cy.get('[value="Mrs"]'),
+    getInputName: () => cy.get("#name"),
+    getInputEmail: () =>
+      cy.get("label[for='email']").parent().find("input").last(),
+    getInputPassword: () => cy.get("input[data-qa='password']"),
+    getInputDay: () => cy.get("#days"),
+    getInputMonth: () => cy.get("#months"),
+    getInputYear: () => cy.get("#years"),
+    getCheckboxNewsletter: () => cy.get("#newsletter"),
+    getCheckboxSpecialOffer: () => cy.get("#optin"),
+    getNewsletterCheckbox: () => cy.get("#newsletter"),
+    getSpecialOffersCheckbox: () => cy.get("#optin"),
+
+    // locators for address form:
+    getFirstNameInput: () => cy.get("#first_name"),
+    getLastNameInput: () => cy.get("#last_name"),
+    getCompanyInput: () => cy.get("#company"),
+    getAddress1Input: () => cy.get("#address1"),
+    getAddress2Input: () => cy.get("#address2"),
+    getCountrySelect: () => cy.get("#country"),
+    getStateInput: () => cy.get("#state"),
+    getCityInput: () => cy.get("#city"),
+    getZipInput: () => cy.get("#zipcode"),
+    getMobileInput: () => cy.get("#mobile_number"),
+    getCreateAccountButton: () => cy.get("[data-qa='create-account']"),
+
+    getContinueButton: () => cy.get('[data-qa="continue-button"]'),
+    getLoggedinAs: () => cy.get(".fa-user").parent(),
+    getDeleteAccountButton: () => cy.get('a[href*="/delete_account"]'),
+    getAccountDeletedHeader: () => cy.get('[data-qa="account-deleted"]'),
   };
 
   // methods
   clickSignUpOrLoginLink = () => {
     this.locators.getSignUpOrLoginLink().click();
   };
-  fillOutSignUpForm = (name, email) => {
+
+  fillSignUpForm = (name, email) => {
     this.locators.getSignUpName().type(name);
     this.locators.getSignUpEmail().type(email);
     this.locators.getSignUpButton().click();
+  };
+
+  enterAccountInfo = ({
+    title,
+    fullname,
+    email,
+    password,
+    dob,
+    subscribe,
+    offers,
+  }) => {
+    const { day, month, year } = dob;
+    if (title === "Mr.") this.locators.getTitleMr().check();
+    if (title === "Mrs.") this.locators.getTitleMrs().check();
+
+    this.locators.getInputName().should("have.value", fullname);
+    this.locators.getInputEmail().should("have.value", email);
+    this.locators.getInputPassword().type(password);
+
+    this.locators.getInputDay().select(day);
+    this.locators.getInputMonth().select(month);
+    this.locators.getInputYear().select(year);
+
+    if (subscribe) this.locators.getNewsletterCheckbox().check();
+    if (offers) this.locators.getSpecialOffersCheckbox().check();
+  };
+
+  fillAddressForm = ({
+    firstName,
+    lastName,
+    company,
+    address1,
+    address2,
+    country,
+    state,
+    city,
+    zipcode,
+    mobile,
+  }) => {
+    this.locators.getFirstNameInput().type(firstName);
+    this.locators.getLastNameInput().type(lastName);
+    this.locators.getCompanyInput().type(company);
+    this.locators.getAddress1Input().type(address1);
+
+    if (address2) this.locators.getAddress2Input().type(address2);
+
+    this.locators.getCountrySelect().select(country);
+    this.locators.getStateInput().type(state);
+    this.locators.getCityInput().type(city);
+    this.locators.getZipInput().type(zipcode);
+    this.locators.getMobileInput().type(mobile);
+  };
+
+  checkLoggedinAs = (username) => {
+    this.locators
+      .getLoggedinAs()
+      .should("include.text", `Logged in as ${username}`);
+  };
+
+  deleteAccountandVerify = () => {
+    this.locators.getDeleteAccountButton().click();
+    this.locators.getAccountDeletedHeader().should("be.visible");
   };
 }
